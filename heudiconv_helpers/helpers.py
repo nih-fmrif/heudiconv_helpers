52df4ed6b1cbcfd92a5ea41a184dd825e40f9150
leej3 <johnleenimh@gmail.com>
2018-02-25 10:04:13 -0500

    fix for pep8

--- heudiconv_helpers/helpers.py
+++ heudiconv_helpers/helpers.py
@@ -1,18 +1,16 @@
 from collections import deque
 import numpy as np
-from pathlib import  Path
+from pathlib import Path
 import pandas as pd
 import json
 import platform
 
 
-
-
-def coerce_to_int(num,name):
-    if np.isclose(num,int(num)):
+def coerce_to_int(num, name):
+    if np.isclose(num, int(num)):
         num = int(num)
     else:
-        raise ValueError("%s must be an integer or a rounded float"%name)
+        raise ValueError("%s must be an integer or a rounded float" % name)
     return num
 
 
@@ -37,14 +35,14 @@ def gen_slice_timings(tr, nslices, nvolumes=1, pattern='alt+z'):
         List of floats for slice timing in same units as tr.
         np.nan is returned if any of the arguments are nan.
     """
-    if any(np.isnan([tr,nslices,nvolumes])):
+    if any(np.isnan([tr, nslices, nvolumes])):
         return np.nan
-    tr = coerce_to_int(tr,"tr")
-    nslices = coerce_to_int(nslices,"nslices")
-    nvolumes = coerce_to_int(nvolumes,"nvolumes")
+    tr = coerce_to_int(tr, "tr")
+    nslices = coerce_to_int(nslices, "nslices")
+    nvolumes = coerce_to_int(nvolumes, "nvolumes")
 
-    ordered_times = [tt for tt in np.linspace(0, tr, nslices+1)][:-1]
-    middle = int((nslices % 2) + len(ordered_times)/2)
+    ordered_times = [tt for tt in np.linspace(0, tr, nslices + 1)][:-1]
+    middle = int((nslices % 2) + len(ordered_times) / 2)
     first_half = ordered_times[:middle]
     second_half = ordered_times[middle:]
 
@@ -76,12 +74,12 @@ def gen_slice_timings(tr, nslices, nvolumes=1, pattern='alt+z'):
     elif pattern == 'seq-z' or pattern == 'seqminus':
         tr_timings = ordered_times[::-1]
     else:
-        raise NotImplementedError('Pattern %s is not implemented yet'%pattern)
+        raise NotImplementedError(
+            'Pattern %s is not implemented yet' % pattern)
 
     output = np.hstack([np.array(tr_timings) + n for n in range(nvolumes)])
     output = list(float(str(np.round(oo, 6))) for oo in output)
     return output
-
 
 
 def write_json(path, data):
@@ -95,15 +93,16 @@ def _get_fields(row, data, fieldnames):
         if isinstance(field, tuple):
 
             for ii, key in enumerate(field):
-                    try:
-                        if ii == (len(field) - 1):
-                            # tmp[key] = values_to_set[f_idx]
-                            row = row.append(pd.Series({'_'.join(field): tmp[key]}))
-                        else:
-                            tmp[key]
-                            tmp = tmp[key]
-                    except KeyError:
-                        row = row.append(pd.Series({'_'.join(field): np.nan}))
+                try:
+                    if ii == (len(field) - 1):
+                        # tmp[key] = values_to_set[f_idx]
+                        row = row.append(
+                            pd.Series({'_'.join(field): tmp[key]}))
+                    else:
+                        tmp[key]
+                        tmp = tmp[key]
+                except KeyError:
+                    row = row.append(pd.Series({'_'.join(field): np.nan}))
         else:
             try:
                 row = row.append(pd.Series({field: data[field]}))
@@ -120,14 +119,14 @@ def _set_fields(data, fieldnames, values_to_set):
         if isinstance(field, tuple):
 
             for ii, key in enumerate(field):
-                    try:
-                        if ii == (len(field) - 1):
-                            tmp[key] = values_to_set[f_idx]
-                        else:
-                            tmp[key]
-                            tmp = tmp[key]
-                    except KeyError:
-                            pass
+                try:
+                    if ii == (len(field) - 1):
+                        tmp[key] = values_to_set[f_idx]
+                    else:
+                        tmp[key]
+                        tmp = tmp[key]
+                except KeyError:
+                    pass
         else:
             data[field] = values_to_set[f_idx]
 
@@ -150,18 +149,18 @@ def _del_fields(j_in, fieldnames):
                         tmp[l]
                         tmp = tmp[l]
                 except KeyError:
-                        pass
+                    pass
 #                         print(f, ' not found as a json field')
         else:
             try:
                 del tmp[field]
             except KeyError:
-                        pass
+                pass
 #                         print(f, ' not found as a json field')
 
 
-def modify_json_fields(row,json_col='json_path',fieldnames=None,action='get',
-values_to_set=None):
+def modify_json_fields(row, json_col='json_path', fieldnames=None, action='get',
+                       values_to_set=None):
     """
     For mapping operations with json files across dataframe rows.
 
